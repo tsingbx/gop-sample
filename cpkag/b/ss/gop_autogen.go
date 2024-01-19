cf6f39c13429e0ffd6fc2773532729c13272ef7c
tsingbx <tsingbx@users.noreply.github.com>
2024-01-19 16:25:54 +0800

    init

---
+++ cpkag/b/ss/gop_autogen.go
@@ -0,0 +1,8 @@
+package ss
+
+import "fmt"
+//line cpkag/b/ss/ss.gop:3:1
+func Ab() {
+//line cpkag/b/ss/ss.gop:4:1
+	fmt.Println("ab")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gop-sample
+
+go 1.21
+
+replace github.com/goplus/gop => /Users/xlj/Documents/Github/gop
