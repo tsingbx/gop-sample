3683e8ac483c32279b51a003df677e13de0ee792
tsingbx <tsingbx@users.noreply.github.com>
2024-01-22 10:30:17 +0800

    add for test

--- gop_autogen.go
+++ gop_autogen.go
@@ -6,7 +6,10 @@ import (
 	ab "gop-sample/cpkag/b"
 	"gop-sample/cpkag/b/ss"
 	"gop-sample/cpkag/k"
+	"gop-sample/cpkag/utils"
 	"gop-sample/gpkag"
+	utils1 "gop-sample/utils"
+	utils2 "github.com/liuscraft/testgomod/utils"
 )
 //line a.gop:3:1
 func a() {
@@ -18,46 +21,54 @@ func Ab() {
 //line a.gop:8:1
 	fmt.Println("ab")
 }
-//line b.gop:12
+//line b.gop:16
 func main() {
-//line b.gop:12:1
-	bb := 3
-//line b.gop:13:1
-	Ab()
-//line b.gop:14:1
-	ab.Ab()
-//line b.gop:15:1
-	ab.Ac()
 //line b.gop:16:1
-	ss.Ab()
+	bb := 3
 //line b.gop:17:1
-	fmt.Println(bb)
+	Ab()
 //line b.gop:18:1
-	k.K()
+	ab.Ab()
 //line b.gop:19:1
-	k.Kk()
+	ab.Ac()
 //line b.gop:20:1
-	k.Ab()
+	ss.Ab()
+//line b.gop:21:1
+	ss.Bs()
 //line b.gop:22:1
-	a()
+	fmt.Println(bb)
 //line b.gop:23:1
-	cpkag.P()
+	k.K()
 //line b.gop:24:1
-	cpkag.Gg()
+	k.Kk()
 //line b.gop:25:1
-	gpkag.G()
+	k.Ab()
 //line b.gop:26:1
-	fmt.Println("b")
+	utils2.TestCsgo()
 //line b.gop:27:1
-	fmt.Println("b")
+	utils.TestCsgo()
 //line b.gop:28:1
-	fmt.Println("b")
-//line b.gop:29:1
-	fmt.Println("b")
+	utils1.TestCsgo()
 //line b.gop:30:1
-	fmt.Println("b")
+	a()
 //line b.gop:31:1
-	fmt.Println("b")
+	cpkag.P()
 //line b.gop:32:1
+	cpkag.Gg()
+//line b.gop:33:1
+	gpkag.G()
+//line b.gop:34:1
+	fmt.Println("b")
+//line b.gop:35:1
+	fmt.Println("b")
+//line b.gop:36:1
+	fmt.Println("b")
+//line b.gop:37:1
+	fmt.Println("b")
+//line b.gop:38:1
+	fmt.Println("b")
+//line b.gop:39:1
+	fmt.Println("b")
+//line b.gop:40:1
 	fmt.Println("b")
 }
